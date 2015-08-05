cae2dd278e5b05d4640b2907ac90eec9d44295f6
grumpybear <christopher.metter@informatik.uni-wuerzburg.de>
2015-08-05 13:48:53 +0200

    update dockerfile (in der hoffnung das nun die zweite seite geht)

--- docker/install2nd.sh
+++ docker/install2nd.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-source /build-typo3-app/pre-install-typo3-app.sh
-hook_user_build_script --preinstall
-hook_user_build_script --post-build
+cd /data/www/itc28/Packages/Sites
+git clone git@github.com:lsinfo3/ITC.ITeleTraffic.Main.git Itc.ITeleTrafficMain
+cd /data/www/itc28/
+composer update
+FLOW_CONTEXT=Production ./flow node:repair --node-type TYPO3.Neos.NodeTypes:Page --force && FLOW_CONTEXT=Production ./flow flow:cache:flush --force
